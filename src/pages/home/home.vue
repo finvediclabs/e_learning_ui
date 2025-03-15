cef3ce6a562493b9a824a02039b44454ee953b75
NagalakshmiKarampudi <nagalakshmi.karampudi@finvedic.com>
2025-03-15 18:21:53 +0530

    Home Cards

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -122,9 +122,7 @@ h3 {
   /* margin-left: 5%; */
   /* margin-right: 5%; */
 /* border: 1px solid black; */
-box-shadow: 0px 2px 1px rgba(0, 0, 0, 0.2),
-               0px 1px 1px rgba(0, 0, 0, 0.2),
-               0px 1px 1px rgba(0, 0, 0, 0.2);
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   border-radius: 10px;
   margin-left: auto;
   margin-right: auto;
